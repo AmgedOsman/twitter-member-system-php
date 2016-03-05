e824204aa45fadd539b629baaf430883214d3343
Amged Osman <mamgedosman@gmail.com>
2016-03-05 21:28:27 +0300

    Create global.php

---
+++ public/lang/en/global.php
@@ -0,0 +1,11 @@
+<?php
+$LANG = array();
+$LANG['welcome'] = "%s, Hello & welcome to the website!";
+$LANG['dashboard'] = "Dashboard";
+$LANG['logout'] = "Logout";
+$LANG['login'] = "Login";
+$LANG['account'] = "Account";
+$LANG['profile'] = "Profile";
+$LANG['about'] = "About";
+$LANG['nav'] = "Navigate";
+$LANG['social'] = "Social";

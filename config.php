e2b4fbc6045d828c27116a02f616c6bca38a4dab
Amged Osman <mamgedosman@gmail.com>
2016-03-07 16:33:16 +0300

    Update config.php

--- config.php
+++ config.php
@@ -5,17 +5,17 @@
  * A single location to store configuration.
  */
 
-define('CONSUMER_KEY', '');
-define('CONSUMER_SECRET', '');
-define('OAUTH_CALLBACK', 'http://domain.com/includes/callback.php');
+define('CONSUMER_KEY', 'QiUFzHXBxU6P1PNL0q59b6hdk');
+define('CONSUMER_SECRET', 'BrUQfkVoqPoWFjf2zUrWT4TbZTwups1mE43wCbN9e7wy61CvmE');
+define('OAUTH_CALLBACK', 'http://localhost/twitter/tms/callback');
 
 //-------------------
 // Database
 //---------------------
 	define( 'dbhost', 'localhost' );// this will ususally be 'localhost', but can sometimes differ
-	define( 'dbname', 'dabase_name' );// the name of the database that you are going to use for this project
-	define( 'dbuser', 'user' );// the username that you created, or were given, to access your database
-	define( 'dbpass', 'pass' );// the password that you created, or were given, to access your database
+	define( 'dbname', 'twitter_login' );// the name of the database that you are going to use for this project
+	define( 'dbuser', 'Sacred' );// the username that you created, or were given, to access your database
+	define( 'dbpass', '1234566' );// the password that you created, or were given, to access your database
 
 
 // Get constants.php if it exists - this can be used to
@@ -26,8 +26,7 @@ if ( is_file( dirname( __FILE__ ) . '/constants.php' ) )
 }
 //include "settings.php";
 //include(ROOT_DIR  . 'settings.php');
-require_once(ROOT_DIR . ADMIN_DIR . '/Database.php');
-require_once(ROOT_DIR . ADMIN_DIR . '/classTMS.php');
-
-require_once(ROOT_DIR . ADMIN_DIR . '/TwitterOAuth.php');
-require_once(ROOT_DIR . ADMIN_DIR . '/cTwitterStream.php');
+require_once(CLASSES . 'db/Database.php');
+require_once(CLASSES . 'classTMS.php');
+require_once(CLASSES . 'twitter/TwitterOAuth.php');
+//require_once(ROOT_DIR . CLASSES . 'twitter/cTwitterStream.php');
